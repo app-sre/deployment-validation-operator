691d8b733d8aa7210e51e8412af18fb925ad3883
Rob Rati <rrati@redhat.com>
2020-10-07 13:04:37 -0400

    Combine bundle/operator makefiles This allows usage and definition of common variables for operator build and bundle/catalog builds

--- Makefile
+++ Makefile
@@ -1,7 +1,20 @@
+.EXPORT_ALL_VARIABLES:
+
 GOLANGCI_LINT_CONFIG = .golangci.yml
 IMAGE_REPOSITORY ?= app-sre
 QUAY_USER ?=
 QUAY_TOKEN ?=
+OPM_VERSION = v1.14.0
+
+BUNDLE_DEPLOY_DIR=deploy/bundle
+MANIFEST_DIR=$(BUNDLE_DEPLOY_DIR)/manifests
+CSV=$(MANIFEST_DIR)/deploymentvalidationoperator.clusterserviceversion.yaml
+OPERATOR_IMAGE=$(IMAGE_REGISTRY)/$(IMAGE_REPOSITORY)/$(IMAGE_NAME)
+OPEARTOR_IMAGE_TAG=v$(OPERATOR_VERSION)
+BUNDLE_IMAGE?=$(IMAGE_REGISTRY)/$(IMAGE_REPOSITORY)/$(IMAGE_NAME)-bundle
+CATALOG_IMAGE?=$(IMAGE_REGISTRY)/$(IMAGE_REPOSITORY)/$(IMAGE_NAME)-catalog
+
+CONFIG_DIR=.docker
 
 # This include must go below the above definitions
 include boilerplate/generated-includes.mk
@@ -18,8 +31,33 @@ docker-test:
 	${CONTAINER_ENGINE} run -t $(OPERATOR_IMAGE_URI_TEST)
 
 .PHONY: docker-login-and-push
-docker-login-and-push: docker-build
-	@CONFIG_DIR=`mktemp -d`; \
-	${CONTAINER_ENGINE} --config="$${CONFIG_DIR}" login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io; \
-	${CONTAINER_ENGINE} --config="$${CONFIG_DIR}" push $(OPERATOR_IMAGE_URI); \
-	${CONTAINER_ENGINE} --config="$${CONFIG_DIR}" push $(OPERATOR_IMAGE_URI_LATEST)
+docker-login-and-push: docker-login docker-build
+	${CONTAINER_ENGINE} --config=$(CONFIG_DIR) push $(OPERATOR_IMAGE_URI); \
+	${CONTAINER_ENGINE} --config=$(CONFIG_DIR) push $(OPERATOR_IMAGE_URI_LATEST)
+
+.PHONY: docker-login
+docker-login:
+	mkdir -p $(CONFIG_DIR)
+	${CONTAINER_ENGINE} --config=$(CONFIG_DIR) login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io;
+
+.PHONY: manifest
+manifest:
+	@mkdir -p $(MANIFEST_DIR) ; \
+	TEMPLATE=`mktemp` ; \
+	./$(BUNDLE_DEPLOY_DIR)/generate-csv-template.py > $${TEMPLATE} ; \
+	oc process --local -o yaml --raw=true IMAGE=$(OPERATOR_IMAGE) IMAGE_TAG=$(OPERATOR_IMAGE_TAG) VERSION=$(OPERATOR_VERSION) REPLACE_VERSION=$(REPLACE_VERSION) -f $${TEMPLATE} > $(CSV)
+	@if [ "${REPLACE_VERSION}" == "" ]; then \
+	  sed -i.bak "/replaces/d" $(CSV); \
+	  rm -f $(CSV).bak; \
+	fi
+
+.PHONY: bundle
+bundle: manifest
+	docker build -t ${BUNDLE_IMAGE}:$(CURRENT_COMMIT) $(BUNDLE_DEPLOY_DIR)
+
+.PHONY: catalog
+catalog: docker-login bundle
+	./build_catalog.sh
+
+clean:
+	rm -rf $(MANIFEST_DIR) $(CONFIG_DIR)
--- build_catalog.sh
+++ build_catalog.sh
@@ -10,7 +10,9 @@ count=0
 for var in BUNDLE_IMAGE \
            CATALOG_IMAGE \
            QUAY_USER \
-           QUAY_TOKEN
+           QUAY_TOKEN \
+           CONTAINER_ENGINE \
+           CONFIG_DIR
 do
   if [ ! "${!var}" ]; then
     log "$var is not set"
@@ -20,28 +22,18 @@ done
 
 [ $count -gt 0 ] && exit 1
 
-num_commits=$(git rev-list $(git rev-list --max-parents=0 HEAD)..HEAD --count)
-current_commit=$(git rev-parse --short=7 HEAD)
-version="0.1.$num_commits-$current_commit"
-opm_version="1.14.0"
-
-# Login to docker
-docker_conf="$PWD/.docker"
-mkdir -p "$docker_conf"
-docker_cmd="docker --config=$docker_conf"
-
-$docker_cmd login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+engine_cmd="${CONTAINER_ENGINE} --config=${CONFIG_DIR}"
 
 # Find the CSV version from the previous bundle
-log "Pulling latest bundle image $BUNDLE_IMAGE"
-$docker_cmd pull $BUNDLE_IMAGE:latest && exists=1 || exists=0
+log "Pulling latest tag for bundle $BUNDLE_IMAGE"
+$engine_cmd pull $BUNDLE_IMAGE:latest && exists=1 || exists=0
 
 if [ $exists -eq 1 ]; then
   log "Extracting previous version from bundle image"
-  docker create --name="tmp_$$" $BUNDLE_IMAGE:latest sh
+  ${CONTAINER_ENGINE} create --name="tmp_$$" $BUNDLE_IMAGE:latest sh
   tmp_dir=$(mktemp -d -t sa-XXXXXXXXXX)
   pushd $tmp_dir
-    docker export tmp_$$ | tar -xf -
+    ${CONTAINER_ENGINE} export tmp_$$ | tar -xf -
     prev_version=`find . -name *.clusterserviceversion.* | xargs cat - | python3 -c 'import sys,yaml; print(yaml.safe_load(sys.stdin.read())["metadata"]["name"])'`
     if [[ "$prev_version" == "" ]]; then
       log "Unable to find previous bundle version"
@@ -50,23 +42,23 @@ if [ $exists -eq 1 ]; then
     log "Found previous bundle version $prev_version"
   popd
   rm -rf $tmp_dir
-  docker rm tmp_$$
+  ${CONTAINER_ENGINE} rm tmp_$$
 fi
 
 # Build/push the new bundle
 pushd deploy/bundle
-  log "Creating bundle $BUNDLE_IMAGE:$current_commit"
+  log "Creating bundle $BUNDLE_IMAGE:$CURRENT_COMMIT"
   if [[ $prev_version != "" ]]; then
     export REPLACE_VERSION=$prev_version
   fi
-  export BUNDLE_IMAGE_TAG=$current_commit
-  export OPERATOR_IMAGE_TAG=v$version
-  export VERSION=$version
+  export BUNDLE_IMAGE_TAG=$CURRENT_COMMIT
+#  export OPERATOR_IMAGE_TAG=v$version
+  export VERSION=$OPERATOR_VERSION
   make bundle
-  docker tag $BUNDLE_IMAGE:$current_commit $BUNDLE_IMAGE:latest
+  ${CONTAINER_ENGINE} tag $BUNDLE_IMAGE:$CURRENT_COMMIT $BUNDLE_IMAGE:latest
 
-  log "Pushing the bundle $BUNDLE_IMAGE:$current_commit to repository"
-  $docker_cmd push $BUNDLE_IMAGE:$current_commit
+  log "Pushing the bundle $BUNDLE_IMAGE:$CURRENT_COMMIT to repository"
+  $engine_cmd push $BUNDLE_IMAGE:$CURRENT_COMMIT
   # Do not push the latest tag here.  If there is a problem creating the catalog then
   # pushing the latest tag here will mean subsequent runs will be extracting a bundle
   # version that isn't referenced in the catalog.  This will result in all future
@@ -74,12 +66,12 @@ pushd deploy/bundle
 popd
 
 # Download opm build
-curl -L https://github.com/operator-framework/operator-registry/releases/download/v$opm_version/linux-amd64-opm -o ./opm
+curl -L https://github.com/operator-framework/operator-registry/releases/download/$OPM_VERSION/${GOOS}-${GOARCH}-opm -o ./opm
 chmod u+x ./opm
 
 # Create/push a new catalog via opm
-log "Pulling existing latest catalog $CATALOG_IMAGE"
-$docker_cmd pull $CATALOG_IMAGE:latest && exists=1 || exists=0
+log "Pulling latest tag for catalog $CATALOG_IMAGE"
+$engine_cmd pull $CATALOG_IMAGE:latest && exists=1 || exists=0
 if [ $exists -eq 1 ]; then
   from_arg="--from-index $CATALOG_IMAGE:latest"
 fi
@@ -90,16 +82,16 @@ else
   log "Updating existing catalog $CATALOG_IMAGE"
 fi
 
-./opm index add --bundles $BUNDLE_IMAGE:$current_commit $from_arg --tag $CATALOG_IMAGE:$current_commit --build-tool docker
+./opm index add --bundles $BUNDLE_IMAGE:$CURRENT_COMMIT $from_arg --tag $CATALOG_IMAGE:$CURRENT_COMMIT --build-tool ${CONTAINER_ENGINE}
 if [ $? -ne 0 ]; then
   exit 1
 fi
-docker tag $CATALOG_IMAGE:$current_commit $CATALOG_IMAGE:latest
+${CONTAINER_ENGINE} tag $CATALOG_IMAGE:$CURRENT_COMMIT $CATALOG_IMAGE:latest
 
-log "Pushing catalog $CATALOG_IMAGE:$current_commit to repository"
-$docker_cmd push $CATALOG_IMAGE:$current_commit
+log "Pushing catalog $CATALOG_IMAGE:$CURRENT_COMMIT to repository"
+$engine_cmd push $CATALOG_IMAGE:$CURRENT_COMMIT
 
 # Only put the latest tags once everything else has succeeded
 log "Pushing latest tags for $BUNDLE_IMAGE and $CATALOG_IMAGE"
-$docker_cmd push $CATALOG_IMAGE:latest
-$docker_cmd push $BUNDLE_IMAGE:latest
+$engine_cmd push $CATALOG_IMAGE:latest
+$engine_cmd push $BUNDLE_IMAGE:latest
