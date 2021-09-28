4b024b8840b061c8d78a9ea384ffa0bdad7f8581
Feng Huang <fehuang@redhat.com>
2021-09-28 19:30:00 -0400

    only ignore deployment-like resources owned by deployment-like resources
    Signed-off-by: Feng Huang <fehuang@redhat.com>

--- pkg/utils/object.go
+++ pkg/utils/object.go
@@ -1,8 +1,13 @@
 package utils
 
 import (
+	"golang.stackrox.io/kube-linter/pkg/objectkinds"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
+
+var deploymentLikeMatcher, _ = objectkinds.ConstructMatcher(objectkinds.DeploymentLike)
 
 // IsController returns true if the object passed in does
 // not have a controller associated with it
@@ -11,8 +16,18 @@ func IsController(obj metav1.Object) bool {
 	return controller == nil
 }
 
-// IsOwner returns true if the object passed has no owner references
-func IsOwner(obj metav1.Object) bool {
-	refs := obj.GetOwnerReferences()
-	return len(refs) == 0
+// IsOwner returns false if the object is deployment-like resource and owned by a deployment-like resource
+func IsOwner(obj client.Object) bool {
+	gvk := obj.GetObjectKind().GroupVersionKind()
+	// no need to check owner for none deployment-like resource
+	if deploymentLikeMatcher.Matches(gvk) {
+		for _, ref := range obj.GetOwnerReferences() {
+			refGvk := schema.FromAPIVersionAndKind(ref.APIVersion, ref.Kind)
+			// get a deployment-like owner
+			if deploymentLikeMatcher.Matches(refGvk) {
+				return false
+			}
+		}
+	}
+	return true
 }
