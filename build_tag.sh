4a9418c680cbbe2276e4e479ff3180409be0a964
Feng Huang <fehuang@redhat.com>
2021-10-04 10:27:49 -0400

    fix build tag
    Signed-off-by: Feng Huang <fehuang@redhat.com>

--- build_tag.sh
+++ build_tag.sh
@@ -4,6 +4,6 @@ make \
 OPERATOR_IMAGE_TAG=$(echo $GIT_BRANCH|cut -d"/" -f3) \
 IMAGE_REPOSITORY="deployment-validation-operator" \
 IMAGE_NAME="dv-operator" \
-REGISTRY_USER=$(QUAY_USER) \
-REGISTRY_TOKEN=$(QUAY_TOKEN) \
+REGISTRY_USER=$(echo $QUAY_USER) \
+REGISTRY_TOKEN=$(echo $QUAY_TOKEN) \
 docker-push
