d2bf20270a4dfd0bdcbb1cd82460b8f6d23bc253
Tyler Pate <tpate@redhat.com>
2021-12-03 15:54:52 -0800

    generic_reconciler.go: use gr.reconciledKind over gvk.kind to ensure metrics deletion

--- pkg/controller/generic_reconciler.go
+++ pkg/controller/generic_reconciler.go
@@ -88,7 +88,7 @@ func (gr *GenericReconciler) Reconcile(ctx context.Context, request reconcile.Re
 
 	deleted := err != nil && errors.IsNotFound(err)
 	gvk := instance.GetObjectKind().GroupVersionKind()
-	validations.RunValidations(request, instance, gvk.Kind, deleted)
+	validations.RunValidations(request, instance, gr.reconciledKind, deleted)
 
 	return reconcile.Result{}, nil
 }
