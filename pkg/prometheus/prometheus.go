1968d5926402337a5f569d509267bbfe32c1f58e
Alex Njaastad <anjaasta@redhat.com>
2021-12-09 20:27:14 -0600

    add default collectors

--- pkg/prometheus/prometheus.go
+++ pkg/prometheus/prometheus.go
@@ -6,18 +6,23 @@ import (
 
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
-	"github.com/prometheus/client_golang/prometheus"
+	// Import Prometheus resources
+	. "github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var PrometheusRegistry *prometheus.Registry
+var PrometheusRegistry *Registry
 
 var log = logf.Log.WithName("utils")
 
 func InitMetricsEndpoint(metricsPath string, metricsPort int32) {
-	PrometheusRegistry = prometheus.NewRegistry()
+	PrometheusRegistry = NewRegistry()
+	PrometheusRegistry.MustRegister(NewProcessCollector(ProcessCollectorOpts{}))
+	PrometheusRegistry.MustRegister(NewGoCollector())
+
 	handler := promhttp.HandlerFor(PrometheusRegistry, promhttp.HandlerOpts{})
 	http.Handle(fmt.Sprintf("/%s", metricsPath), handler)
+
 	go func() {
 		err := http.ListenAndServe(fmt.Sprintf(":%d", metricsPort), nil)
 		log.Error(err, "Prometheus metrics server stopped unexpectedly")
