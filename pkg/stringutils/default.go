b4c011682475e2feed0962e913948c4d1cb85de6
Ricardo Lüders <rluders@redhat.com>
2022-10-24 10:53:28 +0200

    fix: remove PointerOrDefault function

--- pkg/stringutils/default.go
+++ pkg/stringutils/default.go
@@ -7,12 +7,3 @@ func OrDefault(s, defaultValue string) string {
 	}
 	return defaultValue
 }
-
-// PointerOrDefault returns the string if it's not nil nor empty, or the default.
-func PointerOrDefault(s *string, defaultValue string) string {
-	if s == nil {
-		return defaultValue
-	}
-
-	return OrDefault(*s, defaultValue)
-}
