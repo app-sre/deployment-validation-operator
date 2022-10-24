ae0eb60328698584968563f057aee47c70c46531
Ricardo Lüders <rluders@redhat.com>
2022-10-24 11:05:30 +0200

    fix: remove stringutils pkg

--- pkg/stringutils/default.go
+++
@@ -1,9 +0,0 @@
-package stringutils
-
-// OrDefault returns the string if it's not empty, or the default.
-func OrDefault(s, defaultValue string) string {
-	if s != "" {
-		return s
-	}
-	return defaultValue
-}
