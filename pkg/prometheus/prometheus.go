75f60fe6576316facd96fce6a341b8f52f2d712d
Isaac Jimeno <ijimeno@redhat.com>
2022-12-01 16:37:29 +0100

    Refactoring Prometheus package (#218)
    * Remove outdated instructions
    
    * Refactor NewServer functionality to include unit tests
    
    * Add unit tests for prometheus' getRouter function

--- pkg/prometheus/prometheus.go
+++ pkg/prometheus/prometheus.go
@@ -18,26 +18,30 @@ type Registry interface {
 	Gather() ([]*dto.MetricFamily, error)
 }
 
+// registerCollectorError is returned by the NewServer method if the Registry
+// causes any error on registering a Collector
+type registerCollectorError struct {
+	collector string
+	trace     error
+}
+
+func (err registerCollectorError) Error() string {
+	return fmt.Sprintf("registering %s collector: %s", err.collector, err.trace.Error())
+}
+
+// NewServer returns a server configured to work on path and address given
+// It registers a collector which exports the current state of process metrics
+// and a collector that exports metrics about the current process
+// It may return registerCollectorError if the collectors are already registered
 func NewServer(registry Registry, path, addr string) (*Server, error) {
 	if !strings.HasPrefix(path, "/") {
 		path = "/" + path
 	}
 
-	var (
-		processCollector = collectors.NewProcessCollector(collectors.ProcessCollectorOpts{})
-		goCollector      = collectors.NewGoCollector()
-	)
-
-	if err := registry.Register(processCollector); err != nil {
-		return nil, fmt.Errorf("registering process collector: %w", err)
+	mux, err := getRouter(registry, path)
+	if err != nil {
+		return nil, err
 	}
-
-	if err := registry.Register(goCollector); err != nil {
-		return nil, fmt.Errorf("registering go collector: %w", err)
-	}
-
-	mux := http.NewServeMux()
-	mux.Handle(path, promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
 
 	return &Server{
 		s: &http.Server{
@@ -46,6 +50,34 @@ func NewServer(registry Registry, path, addr string) (*Server, error) {
 			ReadHeaderTimeout: 2 * time.Second,
 		},
 	}, nil
+}
+
+// getRouter registers two collectors to deliver metrics on a given path
+// It may return registerCollectorError if the collectors are already registered
+func getRouter(registry Registry, path string) (*http.ServeMux, error) {
+	var (
+		processCollector = collectors.NewProcessCollector(collectors.ProcessCollectorOpts{})
+		goCollector      = collectors.NewGoCollector()
+	)
+
+	if err := registry.Register(processCollector); err != nil {
+		return nil, registerCollectorError{
+			collector: "process",
+			trace:     err,
+		}
+	}
+
+	if err := registry.Register(goCollector); err != nil {
+		return nil, registerCollectorError{
+			collector: "go",
+			trace:     err,
+		}
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle(path, promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
+
+	return mux, nil
 }
 
 type Server struct {
