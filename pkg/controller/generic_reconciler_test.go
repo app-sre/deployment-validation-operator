ce4ddbea8a7a2a7889feb3511f994b6f31834a67
Tomáš Remeš <tremes@redhat.com>
2023-04-19 10:12:59 +0200

    DVO-105: extends the logic of matching "app" labels

--- pkg/controller/generic_reconciler_test.go
+++ pkg/controller/generic_reconciler_test.go
@@ -84,88 +84,6 @@ func TestHelperFunctions(t *testing.T) {
 	})
 }
 
-func TestGetAppLabel(t *testing.T) {
-	tests := []struct {
-		testName      string
-		object        runtime.Object
-		expectedLabel string
-		expectedError error
-	}{
-		{
-			testName: "Pod with defined label",
-			object: &v1.Pod{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "app-A",
-					Labels: map[string]string{
-						"app": "app-A",
-					},
-				},
-			},
-			expectedLabel: "app-A",
-			expectedError: nil,
-		},
-		{
-			testName: "Pod with undefined label",
-			object: &v1.Pod{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "empty-app",
-					Namespace: "test",
-				},
-			},
-			expectedLabel: "",
-			expectedError: fmt.
-				Errorf("can't find any 'app' label for empty-app resource from test namespace"),
-		},
-		{
-			testName: "PDB with defined selector label",
-			object: &policyv1.PodDisruptionBudget{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "pdb-A",
-					Namespace: "test",
-				},
-				Spec: policyv1.PodDisruptionBudgetSpec{
-					Selector: &metav1.LabelSelector{
-						MatchLabels: map[string]string{
-							"app": "app-with-pdb",
-						},
-					},
-				},
-			},
-			expectedLabel: "app-with-pdb",
-			expectedError: nil,
-		},
-		{
-			testName: "PDB with empty selector",
-			object: &policyv1.PodDisruptionBudget{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "empty-app",
-					Namespace: "test",
-				},
-			},
-			expectedLabel: "",
-			expectedError: fmt.
-				Errorf("can't find any 'app' label for empty-app resource from test namespace"),
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.testName, func(t *testing.T) {
-			o, err := runtime.DefaultUnstructuredConverter.ToUnstructured(tt.object)
-			assert.NoError(t, err)
-			u := &unstructured.Unstructured{
-				Object: o,
-			}
-			label, err := getAppLabel(u)
-			if tt.expectedError != nil {
-				assert.Error(t, err, tt.expectedError)
-			} else {
-				assert.NoError(t, err)
-			}
-			assert.Equal(t, tt.expectedLabel, label)
-		})
-	}
-}
-
 func TestGroupAppObjects(t *testing.T) {
 	tests := []struct {
 		name          string
@@ -288,6 +206,207 @@ func TestGroupAppObjects(t *testing.T) {
 				"B": {"test-B-pod", "test-B-deployment"},
 			},
 		},
+		{
+			name:      "Four deployments with multiple various matching PDBs",
+			namespace: "test",
+			objs: []client.Object{
+				&appsv1.Deployment{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-deployment-A",
+						Namespace: "test",
+						Labels: map[string]string{
+							"app": "A",
+						},
+					},
+				},
+				&appsv1.Deployment{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-deployment-B",
+						Namespace: "test",
+						Labels: map[string]string{
+							"app": "B",
+						},
+					},
+				},
+				&appsv1.Deployment{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-deployment-C",
+						Namespace: "test",
+						Labels: map[string]string{
+							"app": "C",
+						},
+					},
+				},
+				&appsv1.Deployment{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-deployment-D",
+						Namespace: "test",
+						Labels: map[string]string{
+							"app": "D",
+						},
+					},
+				},
+				&policyv1.PodDisruptionBudget{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-pdb-A-B-C",
+						Namespace: "test",
+					},
+					Spec: policyv1.PodDisruptionBudgetSpec{
+						Selector: &metav1.LabelSelector{
+							MatchExpressions: []metav1.LabelSelectorRequirement{
+								{
+									Key:      "app",
+									Operator: metav1.LabelSelectorOpIn,
+									Values:   []string{"A", "B", "C"},
+								},
+							},
+						},
+					},
+				},
+				&policyv1.PodDisruptionBudget{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-pdb-not-in-C",
+						Namespace: "test",
+					},
+					Spec: policyv1.PodDisruptionBudgetSpec{
+						Selector: &metav1.LabelSelector{
+							MatchExpressions: []metav1.LabelSelectorRequirement{
+								{
+									Key:      "app",
+									Operator: metav1.LabelSelectorOpNotIn,
+									Values:   []string{"C"},
+								},
+							},
+						},
+					},
+				},
+				&policyv1.PodDisruptionBudget{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-pdb-exists",
+						Namespace: "test",
+					},
+					Spec: policyv1.PodDisruptionBudgetSpec{
+						Selector: &metav1.LabelSelector{
+							MatchExpressions: []metav1.LabelSelectorRequirement{
+								{
+									Key:      "app",
+									Operator: metav1.LabelSelectorOpExists,
+								},
+							},
+						},
+					},
+				},
+				&policyv1.PodDisruptionBudget{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-pdb-not-in-D",
+						Namespace: "test",
+					},
+					Spec: policyv1.PodDisruptionBudgetSpec{
+						Selector: &metav1.LabelSelector{
+							MatchExpressions: []metav1.LabelSelectorRequirement{
+								{
+									Key:      "app",
+									Operator: metav1.LabelSelectorOpNotIn,
+									Values:   []string{"D"},
+								},
+							},
+						},
+					},
+				},
+			},
+			gvks: []schema.GroupVersionKind{
+				{
+					Group:   "policy",
+					Kind:    "PodDisruptionBudget",
+					Version: "v1",
+				},
+				{
+					Group:   "apps",
+					Kind:    "Deployment",
+					Version: "v1",
+				},
+			},
+			expectedNames: map[string][]string{
+				"A": {"test-pdb-A-B-C", "test-pdb-not-in-C", "test-pdb-exists", "test-deployment-A", "test-pdb-not-in-D"},
+				"B": {"test-pdb-A-B-C", "test-pdb-not-in-C", "test-pdb-exists", "test-deployment-B", "test-pdb-not-in-D"},
+				"C": {"test-pdb-A-B-C", "test-pdb-exists", "test-deployment-C", "test-pdb-not-in-D"},
+				"D": {"test-deployment-D", "test-pdb-exists", "test-pdb-not-in-C"},
+			},
+		},
+		{
+			name:      "Two StatefulSets with some matching PDBs",
+			namespace: "test",
+			objs: []client.Object{
+				&policyv1.PodDisruptionBudget{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "pdb-not-in-A",
+						Namespace: "test",
+					},
+					Spec: policyv1.PodDisruptionBudgetSpec{
+						Selector: &metav1.LabelSelector{
+							MatchExpressions: []metav1.LabelSelectorRequirement{
+								{
+									Key:      "app",
+									Operator: metav1.LabelSelectorOpNotIn,
+									Values:   []string{"A"},
+								},
+							},
+						},
+					},
+				},
+				&policyv1.PodDisruptionBudget{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "pdb-not-in-B",
+						Namespace: "test",
+					},
+					Spec: policyv1.PodDisruptionBudgetSpec{
+						Selector: &metav1.LabelSelector{
+							MatchExpressions: []metav1.LabelSelectorRequirement{
+								{
+									Key:      "app",
+									Operator: metav1.LabelSelectorOpNotIn,
+									Values:   []string{"B"},
+								},
+							},
+						},
+					},
+				},
+				&appsv1.StatefulSet{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "statefulset-A",
+						Namespace: "test",
+						Labels: map[string]string{
+							"app": "A",
+						},
+					},
+				},
+				&appsv1.StatefulSet{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "statefulset-B",
+						Namespace: "test",
+						Labels: map[string]string{
+							"app": "B",
+						},
+					},
+				},
+			},
+			gvks: []schema.GroupVersionKind{
+				{
+					Group:   "apps",
+					Kind:    "StatefulSet",
+					Version: "v1",
+				},
+				{
+					Group:   "policy",
+					Kind:    "PodDisruptionBudget",
+					Version: "v1",
+				},
+			},
+			expectedNames: map[string][]string{
+				"A": {"statefulset-A", "pdb-not-in-B"},
+				"B": {"statefulset-B", "pdb-not-in-A"},
+			},
+		},
 	}
 
 	for _, tt := range tests {
@@ -301,8 +420,8 @@ func TestGroupAppObjects(t *testing.T) {
 				objects, ok := groupMap[expectedLabel]
 				assert.True(t, ok, "can't find label %s", expectedLabel)
 				actualNames := unstructuredToNames(objects)
-				for _, exoectedName := range expectedNames {
-					assert.Contains(t, actualNames, exoectedName)
+				for _, expectedName := range expectedNames {
+					assert.Contains(t, actualNames, expectedName, "can't find %s for label value %s", expectedName, expectedLabel)
 				}
 			}
 		})
