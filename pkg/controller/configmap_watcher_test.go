8fc9e1d3a0e9c878dfcbe0c265cef03b84a798b1
Isaac Jimeno <ijimeno@redhat.com>
2023-06-05 19:05:58 +0200

    Refactor unit tests to table

--- pkg/controller/configmap_watcher_test.go
+++ pkg/controller/configmap_watcher_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"golang.stackrox.io/kube-linter/pkg/config"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -12,20 +13,59 @@ import (
 )
 
 func TestStaticConfigMapWatcher(t *testing.T) {
-	// Given
-	cm := &corev1.ConfigMap{
-		ObjectMeta: metav1.ObjectMeta{Namespace: configMapNamespace, Name: configMapName},
-		Data:       map[string]string{"disabled-checks": "check,check2"},
-	}
-	client := kubefake.NewSimpleClientset([]runtime.Object{cm}...)
-	mock := ConfigMapWatcher{
-		clientset: client,
+	testCases := []struct {
+		name   string
+		data   string
+		checks config.ChecksConfig
+	}{
+		{
+			name: "kube-linter 'doNotAutoAddDefaults' is gathered from configuration",
+			data: "checks:\n  doNotAutoAddDefaults: true",
+			checks: config.ChecksConfig{
+				DoNotAutoAddDefaults: true,
+			},
+		},
+		{
+			name: "kube-linter 'addAllBuiltIn' is gathered from configuration",
+			data: "checks:\n  addAllBuiltIn: true",
+			checks: config.ChecksConfig{
+				AddAllBuiltIn: true,
+			},
+		},
+		{
+			name: "kube-linter 'exclude' is gathered from configuration",
+			data: "checks:\n  exclude: [\"check1\", \"check2\"]",
+			checks: config.ChecksConfig{
+				Exclude: []string{"check1", "check2"},
+			},
+		},
+		{
+			name: "kube-linter 'include' is gathered from configuration",
+			data: "checks:\n  include: [\"check1\", \"check2\"]",
+			checks: config.ChecksConfig{
+				Include: []string{"check1", "check2"},
+			},
+		},
 	}
 
-	// When
-	test, err := mock.GetStaticKubelinterConfig(context.Background())
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			// Given
+			cm := &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{Namespace: configMapNamespace, Name: configMapName},
+				Data: map[string]string{
+					"deployment-validation-operator-config.yaml": testCase.data,
+				},
+			}
+			client := kubefake.NewSimpleClientset([]runtime.Object{cm}...)
+			mock := ConfigMapWatcher{clientset: client}
 
-	// Assert
-	assert.NoError(t, err)
-	assert.Equal(t, []string{"check", "check2"}, test)
+			// When
+			test, err := mock.GetStaticKubelinterConfig(context.Background())
+
+			// Assert
+			assert.NoError(t, err)
+			assert.Equal(t, testCase.checks, test.Checks)
+		})
+	}
 }
