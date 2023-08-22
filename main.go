9fa2b714a1a1ac569cca3f3247acc9aa7f3e9ea1
Isaac Jimeno <ijimeno@redhat.com>
2023-08-22 13:13:02 +0200

    Fix loglines prefixes

--- main.go
+++ main.go
@@ -96,7 +96,7 @@ func setupManager(log logr.Logger, opts options.Options) (manager.Manager, error
 
 	log.Info("Registering Components")
 
-	log.Info("-> Initialize Prometheus Registry")
+	log.Info("Initialize Prometheus Registry")
 
 	reg := prometheus.NewRegistry()
 	metrics, err := dvoProm.PreloadMetrics(reg)
@@ -104,7 +104,7 @@ func setupManager(log logr.Logger, opts options.Options) (manager.Manager, error
 		return nil, fmt.Errorf("preloading kube-linter metrics: %w", err)
 	}
 
-	log.Info(fmt.Sprintf("-> Initialize Prometheus metrics endpoint on %q", opts.MetricsEndpoint()))
+	log.Info(fmt.Sprintf("Initialize Prometheus metrics endpoint on %q", opts.MetricsEndpoint()))
 
 	srv, err := dvoProm.NewServer(reg, opts.MetricsPath, fmt.Sprintf(":%d", opts.MetricsPort))
 	if err != nil {
@@ -115,7 +115,7 @@ func setupManager(log logr.Logger, opts options.Options) (manager.Manager, error
 		return nil, fmt.Errorf("adding metrics server to manager: %w", err)
 	}
 
-	log.Info("-> Initialize ConfigMap watcher")
+	log.Info("Initialize ConfigMap watcher")
 
 	cmWatcher, err := configmap.NewConfigMapWatcher(cfg)
 	if err != nil {
@@ -126,7 +126,7 @@ func setupManager(log logr.Logger, opts options.Options) (manager.Manager, error
 		return nil, fmt.Errorf("adding configmap watcher to manager: %w", err)
 	}
 
-	log.Info("-> Initialize Validation Engine")
+	log.Info("Initialize Validation Engine")
 
 	validationEngine, err := validations.NewEngine(opts.ConfigFile, cmWatcher, metrics)
 	if err != nil {
@@ -137,7 +137,7 @@ func setupManager(log logr.Logger, opts options.Options) (manager.Manager, error
 		return nil, fmt.Errorf("adding validationEngine to manager: %w", err)
 	}
 
-	log.Info("-> Initialize Reconciler")
+	log.Info("Initialize Reconciler")
 
 	discoveryClient, err := discovery.NewDiscoveryClientForConfig(mgr.GetConfig())
 	if err != nil {
@@ -240,13 +240,13 @@ func kubeClientQPS() (float32, error) {
 }
 
 func initManager(log logr.Logger, opts options.Options, cfg *rest.Config) (manager.Manager, error) {
-	log.Info("-> Initialize Scheme")
+	log.Info("Initialize Scheme")
 	scheme, err := initializeScheme()
 	if err != nil {
 		return nil, fmt.Errorf("initializing scheme: %w", err)
 	}
 
-	log.Info("-> Getting Manager Options")
+	log.Info("Getting Manager Options")
 	mgrOpts, err := getManagerOptions(scheme, opts)
 	if err != nil {
 		return nil, fmt.Errorf("getting manager options: %w", err)
@@ -257,7 +257,7 @@ func initManager(log logr.Logger, opts options.Options, cfg *rest.Config) (manag
 		return nil, fmt.Errorf("getting new manager: %w", err)
 	}
 
-	log.Info("-> Adding Healthz and Readyz checks")
+	log.Info("Adding Healthz and Readyz checks")
 	if err := mgr.AddHealthzCheck("health", healthz.Ping); err != nil {
 		return nil, fmt.Errorf("adding healthz check: %w", err)
 	}
