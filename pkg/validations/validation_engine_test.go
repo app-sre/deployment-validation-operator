238379972db05971b680b99a82e24bf0c8adb7d3
Tomáš Remeš <tremes@redhat.com>
2023-09-14 15:02:52 +0200

    DVO-141: remove global validationengine variable

--- pkg/validations/validation_engine_test.go
+++ pkg/validations/validation_engine_test.go
@@ -1,10 +1,19 @@
 package validations
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/app-sre/deployment-validation-operator/pkg/testutils"
+	"github.com/prometheus/client_golang/prometheus"
+	promUtils "github.com/prometheus/client_golang/prometheus/testutil"
 	"github.com/stretchr/testify/assert"
+	"golang.stackrox.io/kube-linter/pkg/builtinchecks"
+	"golang.stackrox.io/kube-linter/pkg/checkregistry"
 	"golang.stackrox.io/kube-linter/pkg/config"
+	"golang.stackrox.io/kube-linter/pkg/configresolver"
+	appsv1 "k8s.io/api/apps/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func TestGetValidChecks(t *testing.T) {
@@ -89,4 +98,247 @@ func TestRemoveCheckFromConfig(t *testing.T) {
 			assert.Equal(t, mock.config.Checks, testCase.expected)
 		})
 	}
+}
+
+const (
+	customCheckName        = "test-minimum-replicas"
+	customCheckDescription = "some description"
+	customCheckRemediation = "some remediation"
+	customCheckTemplate    = "minimum-replicas"
+)
+
+func newValidationEngine(configPath string, metrics map[string]*prometheus.GaugeVec) (*validationEngine, error) {
+	config, err := loadConfig(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	ve := &validationEngine{
+		config:  config,
+		metrics: metrics,
+	}
+	loadErr := ve.InitRegistry()
+	if loadErr != nil {
+		return nil, loadErr
+	}
+
+	// checks now are preloaded, adding them after Registry init
+	//ve.metrics = make(map[string]*prometheus.GaugeVec)
+	for _, checkName := range ve.enabledChecks {
+		check := ve.registeredChecks[checkName]
+		ve.metrics[checkName] = newGaugeVecMetric(check)
+	}
+	return ve, nil
+}
+
+func newCustomCheck() config.Check {
+	return config.Check{
+		Name:        customCheckName,
+		Description: customCheckDescription,
+		Remediation: customCheckRemediation,
+		Template:    customCheckTemplate,
+		Scope: &config.ObjectKindsDesc{
+			ObjectKinds: []string{"DeploymentLike"},
+		},
+		Params: map[string]interface{}{"minReplicas": 3},
+	}
+}
+
+func newEngineConfigWithAllChecks() config.Config {
+	return config.Config{
+		CustomChecks: []config.Check{},
+		Checks: config.ChecksConfig{
+			AddAllBuiltIn:        true,
+			DoNotAutoAddDefaults: false,
+		},
+	}
+}
+
+func createTestDeployment(replicas int32) (*appsv1.Deployment, error) {
+	d, err := testutils.CreateDeploymentFromTemplate(
+		testutils.NewTemplateArgs())
+	if err != nil {
+		return nil, err
+	}
+	d.Spec.Replicas = &replicas
+	return &d, nil
+}
+
+func TestUpdateConfig(t *testing.T) {
+	tests := []struct {
+		name          string
+		initialConfig config.Config
+		updatedConfig config.Config
+	}{
+		{
+			name: "basic update test",
+			initialConfig: config.Config{
+				Checks: GetDefaultChecks(),
+			},
+
+			updatedConfig: config.Config{
+				CustomChecks: []config.Check{
+					newCustomCheck(),
+				},
+				Checks: config.ChecksConfig{
+					Include: []string{"foo-a", "foo-b"},
+					Exclude: []string{"exclude-1", "exclude-2"},
+				},
+			},
+		},
+		{
+			name: "setting empty config",
+			initialConfig: config.Config{
+				Checks: GetDefaultChecks(),
+			},
+
+			updatedConfig: config.Config{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ve, err := newValidationEngine("", make(map[string]*prometheus.GaugeVec))
+			assert.NoError(t, err, "failed to create a new validation engine")
+			disableIncompatibleChecks(&tt.initialConfig)
+			assert.Equal(t, tt.initialConfig, ve.config)
+			ve.UpdateConfig(tt.updatedConfig)
+			assert.Equal(t, tt.updatedConfig, ve.config)
+
+		})
+	}
+
+}
+
+func TestRunValidationsForObjects(t *testing.T) {
+	tests := []struct {
+		name                       string
+		initialReplicaCount        int32
+		initialExpectedMetricValue int
+		updatedReplicaCount        int32
+		updatedExpectedMetricValue int
+		runAdditionalValidation    bool
+	}{
+		{
+			name:                       "Custom check (min 3 replicas) is active(failing) and then it's fixed", // nolint: lll
+			initialReplicaCount:        1,
+			initialExpectedMetricValue: 1,
+			updatedReplicaCount:        3,
+			updatedExpectedMetricValue: 0,
+			runAdditionalValidation:    false,
+		},
+		{
+			name:                       "Custom check (min 3 replicas) is not active, then it's failing and it's fixed again", // nolint: lll
+			initialReplicaCount:        0,
+			initialExpectedMetricValue: 0,
+			updatedReplicaCount:        1,
+			updatedExpectedMetricValue: 1,
+			runAdditionalValidation:    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			customCheck := newCustomCheck()
+			metrics := make(map[string]*prometheus.GaugeVec)
+			ve, err := newValidationEngine("test-resources/config-with-custom-check.yaml", metrics)
+			assert.NoError(t, err, "Error creating a new validation engine")
+
+			deployment, err := createTestDeployment(tt.initialReplicaCount)
+			assert.NoError(t, err, "Error creating deployment from template")
+			request := NewRequestFromObject(deployment)
+			request.NamespaceUID = "1234-6789-1011-testUID"
+
+			// run validations with "broken" (replica=1) deployment object
+			_, err = ve.RunValidationsForObjects([]client.Object{deployment}, request.NamespaceUID)
+			assert.NoError(t, err, "Error running validations")
+
+			labels := request.ToPromLabels()
+			metric, err := ve.getMetric(customCheck.Name).GetMetricWith(labels)
+			assert.NoError(t, err, "Error getting prometheus metric")
+
+			expectedConstLabelSubString := fmt.Sprintf(""+
+				"constLabels: {check_description=\"%s\",check_remediation=\"%s\"}",
+				customCheck.Description,
+				customCheck.Remediation,
+			)
+			assert.Contains(t, metric.Desc().String(), expectedConstLabelSubString,
+				"Metric is missing expected constant labels! Expected:\n%s\nGot:\n%s",
+				expectedConstLabelSubString,
+				metric.Desc().String())
+			metricValue := int(promUtils.ToFloat64(metric))
+			assert.Equal(t, tt.initialExpectedMetricValue, metricValue,
+				"Deployment test failed %#v: got %d want %d",
+				customCheck.Name, metricValue, tt.initialExpectedMetricValue)
+
+			// Problem resolved
+			deployment.Spec.Replicas = &tt.updatedReplicaCount
+			_, err = ve.RunValidationsForObjects([]client.Object{deployment}, request.NamespaceUID)
+			assert.NoError(t, err, "Error running validations")
+			// Metric with label combination should be successfully cleared because problem was resolved.
+			// The 'GetMetricWith()' function will create a new metric with provided labels if it
+			// does not exist. The default value of a metric is 0. Therefore, a value of 0 implies we
+			// successfully cleared the metric label combination.
+			metric, err = ve.getMetric("test-minimum-replicas").GetMetricWith(labels)
+			assert.NoError(t, err, "Error getting prometheus metric")
+
+			metricValue = int(promUtils.ToFloat64(metric))
+			assert.Equal(t, tt.updatedExpectedMetricValue, metricValue,
+				"Deployment test failed %#v: got %d want %d", "test-minimum-replicas",
+				metricValue, tt.updatedExpectedMetricValue)
+
+			if tt.runAdditionalValidation {
+				deployment.Spec.Replicas = &tt.initialReplicaCount
+				_, err = ve.RunValidationsForObjects([]client.Object{deployment}, request.NamespaceUID)
+				assert.NoError(t, err, "Error running validations")
+
+				metric, err = ve.getMetric(customCheck.Name).GetMetricWith(labels)
+				assert.NoError(t, err, "Error getting prometheus metric")
+
+				// If metrics are cleared then the value will be == 0
+				metricValue = int(promUtils.ToFloat64(metric))
+				assert.Equal(t, tt.initialExpectedMetricValue, metricValue,
+					"Deployment test failed %#v: got %d want %d", "test-minimum-replicas",
+					metricValue, tt.initialExpectedMetricValue)
+			}
+		})
+	}
+}
+
+func TestIncompatibleChecksAreDisabled(t *testing.T) {
+	// Initialize engine
+	ve, err := newValidationEngine("test-resources/default-config.yaml", make(map[string]*prometheus.GaugeVec))
+	assert.NoError(t, err, "Error initializing engine")
+
+	badChecks := getIncompatibleChecks()
+	allKubeLinterChecks, err := getAllBuiltInKubeLinterChecks()
+	assert.NoError(t, err, "Got unexpected error while getting all checks built-into kube-linter")
+	expectedNumChecks := (len(allKubeLinterChecks) - len(badChecks))
+
+	enabledChecks := ve.enabledChecks
+	assert.Equal(t, expectedNumChecks, len(enabledChecks),
+		"Expected exactly %v checks to be enabled, but got '%v' checks from list '%v'",
+		expectedNumChecks, len(enabledChecks), enabledChecks)
+
+	for _, badCheck := range badChecks {
+		assert.NotContains(t, enabledChecks, badCheck)
+	}
+}
+
+// getAllBuiltInKubeLinterChecks returns every check built-into kube-linter (including checks that DVO disables)
+func getAllBuiltInKubeLinterChecks() ([]string, error) {
+	ve := validationEngine{
+		config: newEngineConfigWithAllChecks(),
+	}
+	registry := checkregistry.New()
+	if err := builtinchecks.LoadInto(registry); err != nil {
+		return nil, fmt.Errorf("failed to load built-in validations: %s", err.Error())
+	}
+
+	enabledChecks, err := configresolver.GetEnabledChecksAndValidate(&ve.config, registry)
+	if err != nil {
+		return nil, fmt.Errorf("error finding enabled validations: %s", err.Error())
+	}
+
+	return enabledChecks, nil
 }
