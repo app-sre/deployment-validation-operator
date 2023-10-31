6b3d623693276dc6282a64d53a0a86b72a5979e6
Isaac Jimeno <ijimeno@redhat.com>
2023-10-31 18:54:02 +0100

    Add minimal pprof server to manager

---
+++ pkg/pprof/pprof.go
@@ -0,0 +1,44 @@
+package pprof
+
+import (
+	"context"
+	"net/http"
+	_ "net/http/pprof" // this adds all pprof handlers to http.Server
+	"time"
+)
+
+func NewServer() *Pprof {
+	return &Pprof{
+		s: &http.Server{
+			Addr:              "localhost:6060",
+			ReadHeaderTimeout: 2 * time.Second,
+		},
+	}
+}
+
+type Pprof struct {
+	s *http.Server
+}
+
+func (s *Pprof) Start(ctx context.Context) error {
+	errCh := make(chan error)
+
+	drain := func() {
+		for range errCh {
+		}
+	}
+	defer drain()
+
+	go func() {
+		defer close(errCh)
+
+		errCh <- s.s.ListenAndServe()
+	}()
+
+	select {
+	case err := <-errCh:
+		return err
+	case <-ctx.Done():
+		return s.s.Close()
+	}
+}
