c834e58066da30a66a76a47bfc95c90a7a6a37f9
Isaac <ijimeno@redhat.com>
2024-10-01 18:19:58 +0200

    (prev) Temp fix to build_tag script for testing on Jenkins

--- build_tag.sh
+++ build_tag.sh
@@ -8,4 +8,4 @@ IMAGE_REPOSITORY="deployment-validation-operator" \
 IMAGE_NAME="dv-operator" \
 REGISTRY_USER=$(echo $QUAY_USER) \
 REGISTRY_TOKEN=$(echo $QUAY_TOKEN) \
-docker-publish
+test_opm # DEBUG: Rollback after testing
