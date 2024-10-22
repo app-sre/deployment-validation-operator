309cf142f92975ff9cffd7bf9e65f632a37fa098
Isaac <ijimeno@redhat.com>
2024-10-22 10:49:58 +0200

    Remove boilerplate folder

--- boilerplate/_lib/boilerplate.mk
+++
@@ -1,7 +0,0 @@
-.PHONY: boilerplate-commit
-boilerplate-commit:
-	@boilerplate/_lib/boilerplate-commit
-
-.PHONY: boilerplate-freeze-check
-boilerplate-freeze-check:
-	@boilerplate/_lib/freeze-check
